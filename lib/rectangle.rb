6885550f52818d91bef5dea631f7a9dae2ebbf05
Dominic Baggott <dominic.baggott@gmail.com>
2013-08-06 23:11:18 +0100

    Add simple Rectangle shape class

---
+++ lib/rectangle.rb
@@ -0,0 +1,20 @@
+class Rectangle
+  attr_accessor :x1, :y1, :x2, :y2, :color
+
+  def initialize(window, attributes)
+    @window = window
+    attributes.each do |attr, value|
+      send("#{attr}=", value)
+    end
+  end
+
+  def draw
+    @window.draw_quad(x1, y1, color, x2, y1, color,
+                      x1, y2, color, x2, y2, color)
+  end
+
+  def under_mouse?
+    @window.mouse_x >= x1 && @window.mouse_x <= x2 &&
+      @window.mouse_y >= y1 && @window.mouse_y <= y2
+  end
+end
