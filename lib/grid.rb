47d6fbae8138ef404c4747935e80261327f9bba3
Dominic Baggott <dominic.baggott@gmail.com>
2013-08-07 13:08:03 +0100

    Move color references into constants

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,11 +1,15 @@
 require './lib/rectangle.rb'
 
 class Grid
+  CELL_COLOR = Gosu::Color::GRAY
+  CELL_HIGHLIGHT_COLOR = Gosu::Color::RED
+  BORDER_COLOR = Gosu::Color::WHITE
+
   def initialize(window, x, y, columns, rows, tile_size)
     @background = Rectangle.new(window, x1: x, y1: y,
                                 x2: y + columns * tile_size,
                                 y2: y + rows * tile_size,
-                                color: Gosu::Color::WHITE)
+                                color: BORDER_COLOR)
 
     @cells = columns.times.flat_map do |col_index|
       rows.times.map do |row_index|
@@ -14,7 +18,7 @@ class Grid
                       y1: y + row_index * tile_size + 1,
                       x2: x + (col_index + 1) * tile_size - 1,
                       y2: y + (row_index + 1) * tile_size - 1,
-                      color: Gosu::Color::GRAY)
+                      color: CELL_COLOR)
       end
     end
   end
@@ -23,7 +27,7 @@ class Grid
     if @snappable
       @cells.each do |cell|
         # TODO: It seems slightly abusive to update every cell on every update
-        cell.color = cell.under_mouse? ? Gosu::Color::RED : Gosu::Color::GRAY
+        cell.color = cell.under_mouse? ? CELL_HIGHLIGHT_COLOR : CELL_COLOR
       end
     end
   end
@@ -39,6 +43,6 @@ class Grid
 
   def stop_snapping
     @snappable = nil
-    @cells.each { |cell| cell.color = Gosu::Color::GRAY }
+    @cells.each { |cell| cell.color = CELL_COLOR }
   end
 end
