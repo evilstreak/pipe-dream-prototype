27f1b3eff4b525da71358066487a717929d4b22c
Dominic Baggott <dominic.baggott@gmail.com>
2013-08-08 22:33:39 +0100

    Define Point from a struct

--- lib/point.rb
+++ lib/point.rb
@@ -1,11 +1,4 @@
-class Point
-  attr_accessor :x, :y
-
-  def initialize(x, y)
-    @x = x
-    @y = y
-  end
-
+Point = Struct.new(:x, :y) do
   # @returns a new Point offset the given amount from this one
   def offset(x_offset, y_offset)
     Point.new(x + x_offset, y + y_offset)
