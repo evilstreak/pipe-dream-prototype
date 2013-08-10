3b6c16c0ac84444b998619135ec91c732a949529
Dominic Baggott <dominic.baggott@gmail.com>
2013-08-10 15:14:11 +0100

    Add new tile to rack on depletion

--- lib/tile_rack.rb
+++ lib/tile_rack.rb
@@ -3,14 +3,17 @@ require './lib/tile.rb'
 
 class TileRack
   RACK_COLOR = Gosu::Color::GRAY
+  TILE_WIDTH = 96
 
   def initialize(window, top_left, bottom_right, tile_count)
-    @background = Rectangle.from_points(window, top_left, bottom_right, RACK_COLOR)
+    @window = window
+    @background = Rectangle.from_points(@window, top_left, bottom_right,
+                                        RACK_COLOR)
 
-    @tiles = build_tiles(window, top_left, bottom_right, tile_count)
+    @tiles = build_tiles(tile_count)
 
-    window.listen(:mouse_down, method(:on_mouse_down))
-    window.listen(:tile_snap, method(:on_tile_snap))
+    @window.listen(:mouse_down, method(:on_mouse_down))
+    @window.listen(:tile_snap, method(:on_tile_snap))
   end
 
   def draw
@@ -26,18 +29,37 @@ class TileRack
     @tiles.delete(tile)
   end
 
+  def on_tile_snap(snapped_tile)
+    # Replace the snapped tile with a new one in the same position
+    @tiles.map!.with_index do |tile, index|
+      tile == snapped_tile ? build_tile : tile
+    end
+
+    layout_tiles
+  end
+
   private
 
   def under_mouse?
     @background.under_mouse?
   end
 
-  def build_tiles(window, top_left, bottom_right, tile_count)
-    offset = (bottom_right.y - top_left.y) / tile_count
-    point = top_left.offset((bottom_right.x - top_left.x) / 2, offset / 2)
+  def build_tiles(tile_count)
+    @tiles = tile_count.times.map { build_tile }
+    layout_tiles
+  end
 
-    tile_count.times.map do |index|
-      Tile.new(window, point.offset(0, offset * index), 96)
+  def build_tile
+    Tile.new(@window, Point.new(0, 0), TILE_WIDTH)
+  end
+
+  def layout_tiles
+    offset = @background.height / @tiles.count
+    point = @background.top_left.offset((@background.width - TILE_WIDTH) / 2,
+                                        (offset - TILE_WIDTH) / 2)
+
+    @tiles.each.with_index do |tile, index|
+      tile.move_to(point.offset(0, index * offset))
     end
   end
 end
