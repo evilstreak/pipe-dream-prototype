1fbf18bbb271cfeb37fd2b398edcf785777caeac
Dominic Baggott <dominic.baggott@gmail.com>
2013-08-11 22:10:24 +0100

    Add Tile::Block with no pipe

---
+++ lib/tile/block.rb
@@ -0,0 +1,20 @@
+require './lib/tile.rb'
+
+class Tile::Block < Tile
+  public_class_method :new
+
+  private
+
+  def draw_water
+    # No water!
+  end
+
+  def flow_exits
+    # No exits!
+    []
+  end
+
+  def top_layer_image
+    'pipe-block.png'
+  end
+end
