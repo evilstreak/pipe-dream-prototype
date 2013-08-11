5cfe2b130e6c229f018cf343d72aa9a59212185d
Dominic Baggott <dominic.baggott@gmail.com>
2013-08-11 18:23:01 +0100

    Fix water flow animation for other entry sides
    It always showed the left->right (for a straight) or left->bottom (for a
    corner) animation. Now it shows an animation correct for the side the
    water entered from.

--- lib/tile/corner.rb
+++ lib/tile/corner.rb
@@ -6,16 +6,32 @@ class Tile::Corner < Tile
   private
 
   def draw_water
-    progress = @flow_progress / FLOW_SPEED
-    if progress <= 0.5
-      @window.draw_triangle(left, top, WATER_COLOR,
-                            left + width * progress * 2, top, WATER_COLOR,
-                            left, bottom, WATER_COLOR)
-    else
-      @window.draw_quad(left, top, WATER_COLOR,
-                        right, top, WATER_COLOR,
-                        left, bottom, WATER_COLOR,
-                        right, top + width * (progress - 0.5) * 2, WATER_COLOR)
+    if @flow_progress > 0
+      progress = @flow_progress / FLOW_SPEED
+
+      if progress <= 0.5
+        if @flow_entry_side == :left
+          @window.draw_triangle(left, top, WATER_COLOR,
+                                left + width * progress * 2, top, WATER_COLOR,
+                                left, bottom, WATER_COLOR)
+        else
+          @window.draw_triangle(right, bottom, WATER_COLOR,
+                                right, bottom - width * progress * 2, WATER_COLOR,
+                                left, bottom, WATER_COLOR)
+        end
+      else
+        if @flow_entry_side == :left
+          @window.draw_quad(left, top, WATER_COLOR,
+                            right, top, WATER_COLOR,
+                            left, bottom, WATER_COLOR,
+                            right, top + width * (progress - 0.5) * 2, WATER_COLOR)
+        else
+          @window.draw_quad(right, bottom, WATER_COLOR,
+                            right, top, WATER_COLOR,
+                            left, bottom, WATER_COLOR,
+                            right - width * (progress - 0.5) * 2, top, WATER_COLOR)
+        end
+      end
     end
   end
 
--- lib/tile/straight.rb
+++ lib/tile/straight.rb
@@ -6,11 +6,20 @@ class Tile::Straight < Tile
   private
 
   def draw_water
-    right_side = left + width * @flow_progress / FLOW_SPEED
-    @window.draw_quad(left, top, WATER_COLOR,
-                      right_side, top, WATER_COLOR,
-                      left, bottom, WATER_COLOR,
-                      right_side, bottom, WATER_COLOR)
+    if @flow_progress > 0
+      if @flow_entry_side == :left
+        entry_side = left
+        exit_side = left + width * @flow_progress / FLOW_SPEED
+      else
+        entry_side = right
+        exit_side = right - width * @flow_progress / FLOW_SPEED
+      end
+
+      @window.draw_quad(entry_side, top, WATER_COLOR,
+                        exit_side, top, WATER_COLOR,
+                        entry_side, bottom, WATER_COLOR,
+                        exit_side, bottom, WATER_COLOR)
+    end
   end
 
   def flow_exits
