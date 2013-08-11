42ecc726d95be46b31f9ba82392c1972bb9383af
Dominic Baggott <dominic.baggott@gmail.com>
2013-08-11 08:45:23 +0100

    Build columns not rows in Grid
    When we start scrolling we're going to need to add more columns onto the
    Grid as we progress, so let's stick with an approach that matches that.

--- lib/grid.rb
+++ lib/grid.rb
@@ -6,8 +6,8 @@ class Grid
 
   def initialize(window, top_left, columns, rows, cell_size)
     @window = window
-    @cells = rows.times.flat_map do |row_index|
-      build_row(top_left.offset(0, row_index * cell_size), columns, cell_size)
+    @cells = columns.times.flat_map do |col_index|
+      build_column(top_left.offset(col_index * cell_size, 0), rows, cell_size)
     end
   end
 
@@ -17,10 +17,10 @@ class Grid
 
   private
 
-  def build_row(top_left, cell_count, cell_size)
+  def build_column(top_left, cell_count, cell_size)
     center = top_left.offset(cell_size / 2, cell_size / 2)
     cell_count.times.map do |cell_index|
-      Cell.new(@window, center.offset(cell_index * cell_size, 0), cell_size)
+      Cell.new(@window, center.offset(0, cell_index * cell_size), cell_size)
     end
   end
 end
